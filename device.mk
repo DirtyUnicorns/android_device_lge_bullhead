28db9cf76b928094a4a281f548d2d8ae5da7b1ef
Sam Mortimer <sam@mortimer.me.uk>
2015-11-13 18:19:03 -0500

    Bullhead: Enable Charging LED Support
    Change-Id: I7004b9067462c7114c290576362d4c1114c4df17

--- device.mk
+++ device.mk
@@ -406,6 +406,7 @@ PRODUCT_PACKAGES += \
 
 # set default USB configuration
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    sys.usb.config=mtp,adb \
     persist.sys.usb.config=mtp,adb \
     ro.adb.secure=0
 
