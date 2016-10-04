d9474b6ebbca590fade99c3e4469a8af1e140c25
Josh Chasky <jchasky@gmail.com>
2016-10-04 16:31:33 -0400

    Bullhead: Update build fingerprint to NBD90W
    Change-Id: I1f8d2fb43f5ad8d6086a2d3b0267760537bf26c0

--- du.mk
+++ du.mk
@@ -33,5 +33,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:7.0/NRD90S/3142244:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 7.0 NRD90S 3142244 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys"
