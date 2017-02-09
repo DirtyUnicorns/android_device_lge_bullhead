efa1ba9f851d9e5f16f76b587cc21fae675f32be
Josh Chasky <jchasky@gmail.com>
2017-02-09 08:53:38 -0500

    Bullhead: Enable Google Assistant
    Change-Id: I4188baaa74ca915104c0c3f836ce2aa01d96560f

--- device.mk
+++ device.mk
@@ -500,6 +500,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1﻿
 
+# Google Assistant
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
+
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8992.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
-
