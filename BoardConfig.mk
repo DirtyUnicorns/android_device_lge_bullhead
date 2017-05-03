47e2cac554d407f5e66bd4d42e7b65e3d0334308
nickd81 <nickdoh81@gmail.com>
2017-05-03 13:07:19 -0400

    Use device audio_effects.conf
    - This will ignore the audio_effects.conf from the vendor partition.
    -- Retrofit to the DSP effects
    
    Change-Id: I70b76b0993561d91d2a1eb8e01d46d04e5a74480

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,4 +156,8 @@ USE_CLANG_PLATFORM_BUILD := true
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
 
+# Audio effects for DSP Manager
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
+
 -include vendor/lge/bullhead/BoardConfigVendor.mk
